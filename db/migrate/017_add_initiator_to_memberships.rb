488bd96c6b9f30d9bafbdbec0ad5fe3c1da09815
bborn <bruno.bornsztein@gmail.com>
2009-10-08 14:25:05 -0500

    removing old stuff

--- db/migrate/017_add_initiator_to_memberships.rb
+++
@@ -1,9 +0,0 @@
-class AddInitiatorToMemberships < ActiveRecord::Migration
-  def self.up
-    add_column :memberships, :initiator, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :memberships, :initiator
-  end
-end
